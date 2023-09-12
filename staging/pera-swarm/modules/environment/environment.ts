8c6d46ff32303003ae61a9a855003890ab5b1492
Kavinaya12 <e17159@eng.pdn.ac.lk>
2023-09-12 11:27:30 +0530

    refactored

--- staging/pera-swarm/modules/environment/environment.ts
+++ staging/pera-swarm/modules/environment/environment.ts
@@ -47,7 +47,7 @@ export abstract class AbstractEnvironment {
         this._obstacleController = obstacleController;
         this._config = config;
         this._updated = Date.now();
-        this._config.arena = config.arena;
+        // this._config.arena = config.arena;
     }
 
     /**
